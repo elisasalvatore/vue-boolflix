37cf785d2aa465e3759d738f7b8f5ab164abf5ee
ElisaSalvatore <elisa.salvatore110@gmail.it>
2022-02-03 12:13:01 +0100

    basic structure

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+    <div class=title>Boolflix</div>
+    <input type="text" placeholder="Search"> 
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="scss">
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 50px;
+  padding: 0 20px;
+  border: 2px solid black;
+}
+</style>
---
+++ src/components/MainContainer.vue
@@ -0,0 +1,18 @@
+<template>
+    <main>
+
+    </main>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+main {
+    height: 100vh;
+    background-color:antiquewhite;
+}
+</style>
