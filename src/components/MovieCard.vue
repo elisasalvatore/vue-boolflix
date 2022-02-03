be2018257b93b085d014111dc54ed001f8749437
ElisaSalvatore <elisa.salvatore110@gmail.it>
2022-02-03 18:33:07 +0100

    added 'image not avaible' if poster_path is 'null'

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -2,7 +2,14 @@
     <div class="container-cards">
         <div class="cards">
             <img :src="getMoviePoster()" alt="">
-            <!-- <div class="title">{{movie.original_title}}</div>  NELL'HOVER-->
+
+            <!-- <div class="cards-hover">
+                <div class="title">Titolo: {{movie.title}}</div>
+                <div class="original-title">Titolo originale: {{movie.original_title}}</div>
+                <div class="language">Lingua originale: {{movie.original_language}}</div>
+                <div class="vote">Voto: {{movie.vote_average}}</div>
+                <div class="overview">Overflow: {{movie.overview}}</div>
+            </div> -->
         </div>
     </div>
 </template>
@@ -14,6 +21,9 @@ export default {
     },
     methods: {
         getMoviePoster() {
+            if (this.movie.poster_path === null) {
+                return `https://www.wanted.gr/wp-content/uploads/2021/11/Image-Not-Available.png`
+            }
             return `https://image.tmdb.org/t/p/w200/${this.movie.poster_path}`
         }
     }
@@ -23,15 +33,27 @@ export default {
 <style lang="scss" scoped>
 .container-cards {
     margin-top: 20px;
-    
-    .cards {
-    
-    padding: 10px;
 
-    img {
-        // width: 200px;
-        height: 300px;
+    .cards{
+        padding: 10px;
+
+        img {
+            // width: 200px;
+            height: 300px;
+        }
     }
-}
+
+    // .cards-hover {
+    //     width: 200px;
+    //     height: 300px;
+    //     padding: 15px;
+    //     color: white;
+    //     background-color: black;
+    //     border: 2px solid #fff;
+        
+    //     > div {
+    //         margin: 5px 0;
+    //     }
+    // }
 }
 </style>
